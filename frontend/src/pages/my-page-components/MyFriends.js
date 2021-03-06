7031a8b5ede05e4883394019be2bed7d81c7a55c
ayberkuslu <usluayberk1998@gmail.com>
2021-03-06 22:13:05 +0300

    dummy data removed

--- frontend/src/pages/my-page-components/MyFriends.js
+++ frontend/src/pages/my-page-components/MyFriends.js
@@ -41,118 +41,14 @@ export class MyFriends extends React.Component {
                     {  "id" : 0,"name": "Ayberk", "surname" : "Uslu", "Age": 22, "withFriendsSince" : "15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
                     {  "id" : 1, "name": "Ayberk2", "surname" : "Uslu2", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
                     {  "id" : 2, "name": "Ayberk3", "surname" : "Uslu3", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
-                    {  "id" : 3, "name": "Ayberk4", "surname" : "Uslu4", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]}
+                    {  "id" : 3, "name": "Ayberk4", "surname" : "Uslu4", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
+                    {  "id" : 3, "name": "Ayberk5", "surname" : "Uslu5", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
+                    {  "id" : 3, "name": "Ayberk6", "surname" : "Uslu6", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
+                    {  "id" : 3, "name": "Ayberk7", "surname" : "Uslu7", "Age": 22, "withFriendsSince" :"15.02.2021", "vaccines" : [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]} ,
                 ]
         };
 
-    // let test_data = {
-    //     "root": [
-    //         {
-    //             "key": "0",
-    //             "data": {"name": "Applications", "size": "100kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "0-0",
-    //                 "data": {"name": "React", "size": "25kb", "type": "Folder"},
-    //                 "children": [{
-    //                     "key": "0-0-0",
-    //                     "data": {"name": "react.app", "size": "10kb", "type": "Application"}
-    //                 }, {
-    //                     "key": "0-0-1",
-    //                     "data": {"name": "native.app", "size": "10kb", "type": "Application"}
-    //                 }, {"key": "0-0-2", "data": {"name": "mobile.app", "size": "5kb", "type": "Application"}}]
-    //             }, {"key": "0-1", "data": {"name": "editor.app", "size": "25kb", "type": "Application"}}, {
-    //                 "key": "0-2",
-    //                 "data": {"name": "settings.app", "size": "50kb", "type": "Application"}
-    //             }]
-    //         },
-    //         {
-    //             "key": "1",
-    //             "data": {"name": "Cloud", "size": "20kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "1-0",
-    //                 "data": {"name": "backup-1.zip", "size": "10kb", "type": "Zip"}
-    //             }, {"key": "1-1", "data": {"name": "backup-2.zip", "size": "10kb", "type": "Zip"}}]
-    //         },
-    //         {
-    //             "key": "2",
-    //             "data": {"name": "Desktop", "size": "150kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "2-0",
-    //                 "data": {"name": "note-meeting.txt", "size": "50kb", "type": "Text"}
-    //             }, {"key": "2-1", "data": {"name": "note-todo.txt", "size": "100kb", "type": "Text"}}]
-    //         },
-    //         {
-    //             "key": "3",
-    //             "data": {"name": "Documents", "size": "75kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "3-0",
-    //                 "data": {"name": "Work", "size": "55kb", "type": "Folder"},
-    //                 "children": [{
-    //                     "key": "3-0-0",
-    //                     "data": {"name": "Expenses.doc", "size": "30kb", "type": "Document"}
-    //                 }, {"key": "3-0-1", "data": {"name": "Resume.doc", "size": "25kb", "type": "Resume"}}]
-    //             }, {
-    //                 "key": "3-1",
-    //                 "data": {"name": "Home", "size": "20kb", "type": "Folder"},
-    //                 "children": [{"key": "3-1-0", "data": {"name": "Invoices", "size": "20kb", "type": "Text"}}]
-    //             }]
-    //         },
-    //         {
-    //             "key": "4",
-    //             "data": {"name": "Downloads", "size": "25kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "4-0",
-    //                 "data": {"name": "Spanish", "size": "10kb", "type": "Folder"},
-    //                 "children": [{
-    //                     "key": "4-0-0",
-    //                     "data": {"name": "tutorial-a1.txt", "size": "5kb", "type": "Text"}
-    //                 }, {"key": "4-0-1", "data": {"name": "tutorial-a2.txt", "size": "5kb", "type": "Text"}}]
-    //             }, {
-    //                 "key": "4-1",
-    //                 "data": {"name": "Travel", "size": "15kb", "type": "Text"},
-    //                 "children": [{
-    //                     "key": "4-1-0",
-    //                     "data": {"name": "Hotel.pdf", "size": "10kb", "type": "PDF"}
-    //                 }, {"key": "4-1-1", "data": {"name": "Flight.pdf", "size": "5kb", "type": "PDF"}}]
-    //             }]
-    //         },
-    //         {
-    //             "key": "5",
-    //             "data": {"name": "Main", "size": "50kb", "type": "Folder"},
-    //             "children": [{"key": "5-0", "data": {"name": "bin", "size": "50kb", "type": "Link"}}, {
-    //                 "key": "5-1",
-    //                 "data": {"name": "etc", "size": "100kb", "type": "Link"}
-    //             }, {"key": "5-2", "data": {"name": "var", "size": "100kb", "type": "Link"}}]
-    //         },
-    //         {
-    //             "key": "6",
-    //             "data": {"name": "Other", "size": "5kb", "type": "Folder"},
-    //             "children": [{"key": "6-0", "data": {"name": "todo.txt", "size": "3kb", "type": "Text"}}, {
-    //                 "key": "6-1",
-    //                 "data": {"name": "logo.png", "size": "2kb", "type": "Picture"}
-    //             }]
-    //         },
-    //         {
-    //             "key": "7",
-    //             "data": {"name": "Pictures", "size": "150kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "7-0",
-    //                 "data": {"name": "barcelona.jpg", "size": "90kb", "type": "Picture"}
-    //             }, {"key": "7-1", "data": {"name": "primeng.png", "size": "30kb", "type": "Picture"}}, {
-    //                 "key": "7-2",
-    //                 "data": {"name": "prime.jpg", "size": "30kb", "type": "Picture"}
-    //             }]
-    //         },
-    //         {
-    //             "key": "8",
-    //             "data": {"name": "Videos", "size": "1500kb", "type": "Folder"},
-    //             "children": [{
-    //                 "key": "8-0",
-    //                 "data": {"name": "primefaces.mkv", "size": "1000kb", "type": "Video"}
-    //             }, {"key": "8-1", "data": {"name": "intro.avi", "size": "500kb", "type": "Video"}}]
-    //         }
-    //     ]
-    // }
+
 
 
     this.setState({friends : data["friends"]});
@@ -192,10 +88,6 @@ export class MyFriends extends React.Component {
 
 
     render() {
-        // let friends_header = <div className="p-clearfix" style={{'lineHeight': '1.87em'}}>
-        //     Submissions for {(this.state.selectedFormDetail) ? this.state.selectedFormDetail : ""}
-        // </div>;
-
         return (<div>
 
 
@@ -223,11 +115,11 @@ export class MyFriends extends React.Component {
                     expandedRows={this.state.expanded_friends_row}
                     onRowToggle={(e) => this.setState({expanded_friends_row: e.data})}
                     onRowExpand={this.onRowExpand} onRowCollapse={this.onRowCollapse}
-                    emptyMessage="There is no submission yet!"
+                    emptyMessage="There is no friends yet!"
                 >
                     <Column  expander></Column>
                     <Column field="id" header="ID" ></Column>
-                    <Column field="name" header="Name" ></Column>
+                    <Column field="name" header="Name" filter filterPlaceholder="Search by name" filterMatchMode="contains"></Column>
                     <Column field="surname" header="Size"></Column>
                     {/*<Column field="type" header="Type"></Column>*/}
                 </DataTable>
