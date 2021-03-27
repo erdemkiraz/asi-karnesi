e1909fef960c8085cd96ac18e8e4faf129974ae4
elifbasak <yildirimelifbasak@gmail.com>
2021-03-27 21:53:37 +0300

    Aşı  Bilgilerinin listelenmesi ve Permission Butonu eklenmesi

---
+++ mobile-app/QRCodeApp/QRCodeApp/VaccinePermissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VaccinePermissionViewController.swift
+//  QRCodeApp
+//
+//  Created by Elif Basak  Yildirim on 27.03.2021.
+//
+
+import UIKit
+
+class VaccinePermissionViewController: UIViewController {
+    var asi_id : Int = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
