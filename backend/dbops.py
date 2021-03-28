cf266671cec7657984e1112b9e8e8e478002bea8
erdem <erdem@elliot.ai>
2021-03-28 16:37:34 -0400

    Backend: use real data

--- backend/dbops.py
+++ backend/dbops.py
@@ -1,5 +1,14 @@
-from dbconf import session
-from models import User, Country
+from backend.dbconf import session
+from backend.models import (
+    FriendRequest,
+    User,
+    Country,
+    Admin,
+    Friendship,
+    Vaccination,
+    VaccinationStatusRequest,
+    Vaccine,
+)
 
 
 def add_user(name, *, country_id=None, visibility=None):
@@ -32,3 +41,70 @@ def add_country(name):
 def get_country_by_name(name):
     query = session.query(Country).filter(Country.name.like("%" + name + "%"))
     return query.first()
+
+
+def add_friend(user_id1, user_id2):
+    row1 = Friendship(user_id1=user_id1, user_id2=user_id2)
+    row2 = Friendship(user_id1=user_id2, user_id2=user_id1)
+    session.add(row1)
+    session.add(row2)
+    session.commit()
+
+
+def add_friend_request(user_id1, user_id2):
+    row = FriendRequest(requester_id=user_id1, requestee_id=user_id2)
+    session.add(row)
+    session.commit()
+
+def delete_friend_request(request_id):
+    session.query(FriendRequest).filter(request_id).delete()
+    session.commit()
+
+
+def get_active_friend_requests_to_user(user_id):
+    query = session.query(FriendRequest).filter(FriendRequest.requestee_id == user_id)
+    return query.all()
+
+
+def get_friend_request(request_id):
+    query = session.query(FriendRequest).filter(FriendRequest.id == request_id)
+    return query.first()
+
+
+def is_friend(user_id1, user_id2):
+    query = session.query(Friendship).filter(
+        Friendship.user_id1 == user_id1, Friendship.user_id2 == user_id2
+    )
+    return query.first() is not None
+
+
+def get_admin_from_user_safe(user_id):
+    query = session.query(Admin).filter(Admin.user_id == user_id)
+    return query.first()
+
+
+def has_granted_vaccination_request(requester_id, requestee_id):
+    query = session.query(VaccinationStatusRequest).filter(
+        VaccinationStatusRequest.requester_id == requester_id,
+        VaccinationStatusRequest.requestee_id == requestee_id,
+    )
+    status = query.first()
+    return status is not None and status.has_granted
+
+
+def get_friend_ids(user_id):
+    query = session.query(Friendship.user_id2).filter(Friendship.user_id1 == user_id)
+    friends = query.all()
+    friend_ids = [x[0] for x in friends]
+    return friend_ids
+
+
+def get_user_vaccinations(user_id):
+    query = session.query(Vaccination).filter(Vaccination.user_id == user_id)
+    vaccinations = query.all()
+    return vaccinations
+
+
+def get_vaccine(vaccine_id):
+    query = session.query(Vaccine).filter(Vaccine.id == vaccine_id)
+    return query.one_or_none()
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,29 @@
-from hello import app
-import dbops
-import json
+from flask import jsonify, request
 
-from flask import request, jsonify, Flask
-app = Flask(__name__)
+from backend.util import (
+    get_user_all_friend_dicts,
+    get_user_all_friend_request_dicts,
+    get_user_all_vaccination_dicts,
+)
+from backend.hello import app
+from backend.dbops import dbops
 
 
-def run():
-    app.run()
-
-
-def get_result(res, status):
+def get_response(res, status):
     result = jsonify(res)
     result.status_code = status
+
+    result.headers.add(
+        "Access-Control-Allow-Origin", "*"
+    )  # needed for avoiding CORS policy
+    result.headers.add(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, Content-Type, Accept, Origin, Authorization",
+    )
+    result.headers.add(
+        "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    )
+
     return result
 
 
@@ -21,195 +32,237 @@ def add_user():
     name = request.json.get("name")
     country_id = request.json.get("country_id")
     dbops.add_user(name, country_id=country_id)
-    return get_result({}, 200)
+    return get_response({}, 200)
 
 
 @app.route("/user", methods=["GET"])
 def get_user():
     if "id" not in request.args:
-        return get_result({"error": "No user id provided"}, 400)
+        return get_response({"error": "No user id provided"}, 400)
     id = request.args["id"]
     user = dbops.get_user(id)
     if user is None:
-        return get_result({"error": "User not found"}, 400)
+        return get_response({"error": "User not found"}, 400)
     res = {
         "name": user.name,
         "country_id": user.country_id,
+        "privacy": user.visibility,
     }
-    return get_result(res, 200)
+    return get_response(res, 200)
 
 
 @app.route("/basic_test")
 def hello_world():
     dbops.add_country("turkeyq")
     turkey = dbops.get_country_by_name("turkeyq")
-    print('id:', turkey)
+    print("id:", turkey)
     dbops.add_user("erdemsu", country_id=turkey.id)
     erdem = dbops.get_user_by_name("erdem")
     print(erdem.id, erdem)
-    return set_response("Hello Worldqqqasd!")
+    return get_response("Hello Worldqqqasd!", 200)
 
 
 @app.route("/user/friends", methods=["GET"])
 def get_user_friends():
-    # req = json.loads(request.data)
-    # req = req["data"]
-    # print(req)
+    user_id = request.args["user_id"]
 
-    print("Get users friend from db")
+    friend_dicts = get_user_all_friend_dicts(user_id)
 
-    # TODO:  data format is for test and can change, real data should come from DB. according to google_email
-    #  parameter (this can be also changed) Further info => Ayberk
-    friend_data = {
-        "friends": [
-            {"id": 0, "name": "Ayberk", "surname": "Uslu", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Ayberk"}, {"vaccine": "asi1Ayberk"}, {"vaccine": "asi2Ayberk"}, {"vaccine": "asi3Ayberk"}]},
-            {"id": 1, "name": "Burcu", "surname": "Kose", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Burcu"}, {"vaccine": "asi1Burcu"}, {"vaccine": "asi2Burcu"}, {"vaccine": "asi3Burcu"}]},
-            {"id": 2, "name": "Ceyda", "surname": "Keskin", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Ceyda"}, {"vaccine": "asi1Ceyda"}, {"vaccine": "asi2Ceyda"}, {"vaccine": "asi3Ceyda"}]},
-            {"id": 3, "name": "Derya", "surname": "Dincer", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19"}, {"vaccine": "asi1"}, {"vaccine": "asi2"}, {"vaccine": "asi3"}]},
-            {"id": 3, "name": "Emre", "surname": "Demir", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Emre"}, {"vaccine": "asi1Emre"}, {"vaccineEmre": "asi2"}, {"vaccine": "asi3Emre"}]},
-            {"id": 3, "name": "Ferhat", "surname": "Koc", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Ferhat"}, {"vaccine": "asi1Ferhat"}, {"vaccine": "asi2Ferhat"}, {"vaccine": "asi3Ferhat"}, {"vaccine": "asi4Ferhat"}]},
-            {"id": 3, "name": "Gokhan", "surname": "Mutlu", "Age": 22, "withFriendsSince": "15.02.2021",
-             "vaccines":
-                 [{"vaccine": "covid19Gokhan"}, {"vaccine": "asi1Gokhan"}, {"vaccine": "asi2Gokhan"}]}
-        ]
-    }
+    res = {"friends": friend_dicts}
 
+    return get_response(res, 200)
 
-    return set_response(friend_data)
+    # friend_data = {
+    #     "friends": [
+    #         {
+    #             "id": 0,
+    #             "name": "Ayberk",
+    #             "surname": "Uslu",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Ayberk"},
+    #                 {"vaccine": "asi1Ayberk"},
+    #                 {"vaccine": "asi2Ayberk"},
+    #                 {"vaccine": "asi3Ayberk"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 1,
+    #             "name": "Burcu",
+    #             "surname": "Kose",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Burcu"},
+    #                 {"vaccine": "asi1Burcu"},
+    #                 {"vaccine": "asi2Burcu"},
+    #                 {"vaccine": "asi3Burcu"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 2,
+    #             "name": "Ceyda",
+    #             "surname": "Keskin",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Ceyda"},
+    #                 {"vaccine": "asi1Ceyda"},
+    #                 {"vaccine": "asi2Ceyda"},
+    #                 {"vaccine": "asi3Ceyda"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "Derya",
+    #             "surname": "Dincer",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19"},
+    #                 {"vaccine": "asi1"},
+    #                 {"vaccine": "asi2"},
+    #                 {"vaccine": "asi3"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "Emre",
+    #             "surname": "Demir",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Emre"},
+    #                 {"vaccine": "asi1Emre"},
+    #                 {"vaccineEmre": "asi2"},
+    #                 {"vaccine": "asi3Emre"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "Ferhat",
+    #             "surname": "Koc",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Ferhat"},
+    #                 {"vaccine": "asi1Ferhat"},
+    #                 {"vaccine": "asi2Ferhat"},
+    #                 {"vaccine": "asi3Ferhat"},
+    #                 {"vaccine": "asi4Ferhat"},
+    #             ],
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "Gokhan",
+    #             "surname": "Mutlu",
+    #             "Age": 22,
+    #             "withFriendsSince": "15.02.2021",
+    #             "vaccines": [
+    #                 {"vaccine": "covid19Gokhan"},
+    #                 {"vaccine": "asi1Gokhan"},
+    #                 {"vaccine": "asi2Gokhan"},
+    #             ],
+    #         },
+    #     ]
+    # }
 
-
-@app.route("/add", methods=["POST"])
-def add_new_friend():
-    # req = json.loads(request.data)
-    # req = req["data"]
-    # print(req)
-    print("Get users friend from db")
-
-    # TODO:  data format is for test and can change, real data should come from DB. according to google_email
-    #  parameter (this can be also changed) Further info => Ayberk
-
-    return {"status": 200};
-    # return jsonify(friend_data)
 
 @app.route("/user/codes", methods=["GET"])
 def get_user_codes():
-    # req = json.loads(request.data)
-    # req = req["data"]
-    # print(req)
+    user_id = request.args["user_id"]
 
-    # return "asdas"
-    print("Get user codes  from db")
+    res = {"my_vaccines": get_user_all_vaccination_dicts(user_id)}
 
-    # TODO:  data format is for test and can change, real data should come from DB. according to google_email
-    #  parameter (this can be also changed) Further info => Ayberk
+    return get_response(res, 200)
 
-    code_data = {
-        "my_vaccines": [
-            {
-                "id": 0,
-                "name": "COVID-19",
-                "date": "15.02.2019",
-                "dose": "1",
-                "vaccine_point": "Ankara Merkez",
-                "expires_in": "364",
-            },
-            {
-                "id": 1,
-                "name": "COVID-20",
-                "date": "15.02.2020",
-                "dose": "1",
-                "vaccine_point": "Istranbul Merkez",
-                "expires_in": "255"
-            },
-            {
-                "id": 2,
-                "name": "COVID-21",
-                "date": "15.02.2021",
-                "dose": "1",
-                "vaccine_point": "Eskisehir Merkez",
-                "expires_in": "321"
-            },
-            {
-                "id": 3,
-                "name": "COVID-22",
-                "date": "15.02.2022",
-                "dose": "1",
-                "vaccine_point": "Adana Merkez",
-                "expires_in": "0"
-            }
-        ]
-    }
-    # Serializing json
-    # json_object = json.dumps(code_data)
-    # print(json_object)
-    # print(jsonify(code_data))
-    return set_response(code_data)
+    # code_data = {
+    #     "my_vaccines": [
+    #         {
+    #             "id": 0,
+    #             "name": "COVID-19",
+    #             "date": "15.02.2019",
+    #             "dose": "1",
+    #             "vaccine_point": "Ankara Merkez",
+    #             "expires_in": "364",
+    #         },
+    #         {
+    #             "id": 1,
+    #             "name": "COVID-20",
+    #             "date": "15.02.2020",
+    #             "dose": "1",
+    #             "vaccine_point": "Istranbul Merkez",
+    #             "expires_in": "255",
+    #         },
+    #         {
+    #             "id": 2,
+    #             "name": "COVID-21",
+    #             "date": "15.02.2021",
+    #             "dose": "1",
+    #             "vaccine_point": "Eskisehir Merkez",
+    #             "expires_in": "321",
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "COVID-22",
+    #             "date": "15.02.2022",
+    #             "dose": "1",
+    #             "vaccine_point": "Adana Merkez",
+    #             "expires_in": "0",
+    #         },
+    #     ]
+    # }
 
 
-@app.route("/add-new-friend", methods=["POST"])
-def add_new_friend():
-    # req = json.loads(request.data)
-    # req = req["data"]
-    # print(req)
+@app.route("/friend-request", methods=["POST"])
+def add_friend_request():
+    user_id = request.json["user_id"]
+    friend_id = request.json["friend_id"]
+    dbops.add_friend_request(user_id, friend_id)
 
-    print("Add new friendss")
+    return get_response({}, 200)
 
-    return set_response({"status": 200})
-    # return jsonify(friend_data)
+
+@app.route("/friend-requests", methods=["GET"])
+def get_user_friend_requests():
+    user_id = request.json["user_id"]
+    res = {"friend_requests:": get_user_all_friend_request_dicts(user_id)}
+
+    return get_response(res, 200)
+
+
+@app.route("/accept-friend-request", methods=["PUSH"])
+def accept_friend_request():
+    request_id = request.json["request_id"]
+    accept_friend_request(request_id)
+    return get_response({}, 200)
+
+
+@app.route("/reject-friend-request", methods=["PUSH"])
+def reject_friend_request():
+    request_id = request.json["request_id"]
+    reject_friend_request(request_id)
+    return get_response({}, 200)
 
 
 @app.route("/set-privacy", methods=["POST"])
 def set_vaccine_privacy():
-    print("Set vaccine privacy")
+    user_id = request.json["user_id"]
+    new_privacy = request.json["new_privacy"]
+    user = dbops.get_user(user_id)
+    user.visilibty = new_privacy
 
-    return set_response({"status": 200})
+    return get_response({}, 200)
 
 
 @app.route("/get-privacy", methods=["GET"])
 def get_vaccine_privacy():
-    # req = json.loads(request.data)
-    # req = req["data"]
-    # print(req)
-    vaccine_id = request.args.get('vaccine_id')
-    print(vaccine_id)
-    # 0 means Nobody should see his/her vaccine info . TODO: Calculate from db and send it to frontend!
-    # Nobody : 0
-    # Just Friends : 1
-    # Everybody : 2
-    vaccine_privacy_setting_from_db = "0"
-    users_vaccine_privacy = {
-        "privacy_setting": vaccine_privacy_setting_from_db
-    }
-    response = jsonify(users_vaccine_privacy)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    return set_response(users_vaccine_privacy)
-
-    # return jsonify(users_vaccine_privacy)
+    user_id = request.args["user_id"]
+    user = dbops.get_user(user_id)
+    res = {"privacy_setting": user.visibility}
+    return get_response(res, 200)
 
 
-def set_response(data):
-    response = jsonify(data)
-    response.headers.add('Access-Control-Allow-Origin', '*') ## needed for avoiding CORS policy
-    response.headers.add('Access-Control-Allow-Headers',
-                         'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    return response
-
-=======
 if __name__ == "__main__":
     app.run()
