37d6100971c670e1a8a71e80368a77e647700ba2
erdem <erdem@elliot.ai>
2021-04-04 04:30:30 +0300

    fixes

--- backend/dbconf.py
+++ backend/dbconf.py
@@ -1,19 +1,12 @@
-import sqlite3
-
-from flask import g
-from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-
-from sqlalchemy import create_engine
 
 engine = create_engine(
     "sqlite:///database.db", echo=True, connect_args={"check_same_thread": False}
 )
-
-from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker(bind=engine)
 session = Session()
