bf2d66577854648afe52dd24af0ec12772e1676f
Mehmet Murat Akburak <muratakburakma@gmail.com>
2021-04-06 20:48:51 +0300

    Fix database connection

--- backend/dbconf.py
+++ backend/dbconf.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 Base = declarative_base()
 
 local_db_url = "sqlite:///database.db"
-db_url = os.environ.get("DATABASE_URL", local_db_url)
+db_url = os.environ.get("POSTGRE_DATABASE_URL", local_db_url)
 
 engine = create_engine(
-    db_url, echo=True, connect_args={"check_same_thread": False}
+    db_url, echo=True, #connect_args={"check_same_thread": False}
 )
 
 Session = sessionmaker(bind=engine)
