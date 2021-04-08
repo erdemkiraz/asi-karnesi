806a07cac41fffe0074e9120becc9f99b15598fd
Mehmet Murat Akburak <muratakburakma@gmail.com>
2021-04-08 15:32:29 +0300

    Add user to DB at first login

--- frontend/src/components/GoogleAuth.jsx
+++ frontend/src/components/GoogleAuth.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import GoogleLogin from "react-google-login";
-import { clientId } from "../services/base_service";
+import {BASE_URL, BUILD_HEADER, clientId} from "../services/base_service";
 import { SET_USER } from "../redux/types";
 import {remove_key, put_storage} from "../services/StorageUtil";
+import axios from "axios";
 
 class GoogleAuth extends Component {
     state = {
@@ -20,13 +21,28 @@ class GoogleAuth extends Component {
         this.setState({
             fullname: profile.getName(),
             email: profile.getEmail(),
-            image: profile.getImageUrl()
+            image: profile.getImageUrl(),
+            google_id: profile.getId()
         });
         // Update the State
         this.props.dispatch({
             type: SET_USER,
             payload: this.state
         })
+
+        // Send request to backend to add user
+        let data_to_send = {
+            data: this.state
+        };
+        let url = "https://asi-karnesi-development.herokuapp.com/add_user";
+        const options = {
+            method: "POST",
+            headers: BUILD_HEADER("", ""),
+            data: data_to_send,
+            url,
+        };
+        let response = axios(options);
+        console.log("response: ", response);
         // save the user to the local storage
         put_storage('user', this.state)
         // Redirect to the Homepage
