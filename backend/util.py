86b921bdd41e67955c29b819c594f120999cf60a
erdemkiraz <erdemkiraz@gmail.com>
2021-04-12 00:01:43 +0300

    Fix BE & use google_id & new columns & demo data (#56)

--- backend/util.py
+++ backend/util.py
@@ -2,29 +2,44 @@ import dbops
 import random
 import string
 
-from enums import UserVisibility
+from enums import VaccinationVisibility
 
 
-def can_see_vaccines(user_id, target_user_id):
-    visibility = dbops.get_user_visibility(target_user_id)
+def can_see_vaccines(user_id, target_user_id, vaccination_id):
+    visibility = dbops.get_vaccination(vaccination_id)
 
-    if visibility == UserVisibility.PUBLIC.value:
+    if visibility == VaccinationVisibility.PUBLIC.value:
         return True
 
-    if visibility >= UserVisibility.ALL_ADMINS.value:
+    if visibility >= VaccinationVisibility.ALL_ADMINS.value:
         is_user_admin = dbops.get_admin_from_user_safe(user_id) is not None
         if is_user_admin:
             return True
 
-    if visibility >= UserVisibility.PERMITTED_USER.value:
+    if visibility >= VaccinationVisibility.PERMITTED_USER.value:
         if dbops.has_granted_vaccination_request(user_id, target_user_id):
             return True
 
-    if visibility >= UserVisibility.FRIENDS.value:
+    if visibility >= VaccinationVisibility.FRIENDS.value:
         if dbops.is_friend(user_id, target_user_id):
             return True
 
+    if visibility >= VaccinationVisibility.FACEBOOK_FRIENDS.value:
+        if dbops.is_facebook_friend(user_id, target_user_id):
+            return True
+
     return False
+
+
+def get_datetime_str(dt):
+    """
+    >>> dt = datetime.datetime.now()
+    >>> def get_datetime_str(dt):
+    ...     return dt.strftime("%Y-%m-%d %H:%M:%S")
+    >>> get_datetime_str(dt)
+    '2021-04-11 17:19:10'
+    """
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def get_vaccination_dict(vaccination):
@@ -33,10 +48,10 @@ def get_vaccination_dict(vaccination):
         "vaccination_id": vaccination.id,
         "vaccine_id": vaccination.vaccine_id,
         "name": vaccine.name,
-        "date": vaccination.date,
+        "date": get_datetime_str(vaccination.date),
         "dose": 1,  # this info shouldn't be here
         "vaccine_point": vaccination.vaccinated_at,
-        "valid_until": vaccination.valid_until,
+        "valid_until": get_datetime_str(vaccination.valid_until),
     }
     return res
 
@@ -72,9 +87,9 @@ def get_friend_request_dict(friend_request):
     res = {
         "request_id": friend_request.id,
         "requester_id": requestee_user.id,
-        "requester_email": "requestee_user.email",
+        "requester_email": requestee_user.email,
         "requester_name": requestee_user.name,
-        "created": friend_request.created,
+        "created": get_datetime_str(friend_request.created),
     }
     return res
 
@@ -87,13 +102,10 @@ def get_user_all_friend_request_dicts(user_id):
     return friend_request_dicts
 
 
-def accept_friend_request(request_id):
-    friend_request = dbops.get_friend_request(request_id)
-    dbops.add_friend(friend_request.requester.id, friend_request.requestee_id)
-    dbops.delete_friend_request(request_id)
-
-
-def reject_friend_request(request_id):
+def update_friend_request(request_id, accept):
+    if accept:
+        friend_request = dbops.get_friend_request(request_id)
+        dbops.add_friend(friend_request.requester.id, friend_request.requestee_id)
     dbops.delete_friend_request(request_id)
 
 
@@ -104,7 +116,7 @@ def get_friend_dict(user_id, friend_id):
     if not can_see_vaccines(user_id, friend_id):
         friend_dict["vaccines"] = []
 
-    friend_dict["with_friends_since"] = friendship.created
+    friend_dict["with_friends_since"] = get_datetime_str(friendship.created)
 
     return friend_dict
 
@@ -144,3 +156,11 @@ def get_link_vaccination_ids(link):
     link_id = vaccination_link_row.id
     vaccination_ids = dbops.get_vaccination_ids_from_link_id(link_id)
     return vaccination_ids
+
+
+def create_user(google_id):
+    user = dbops.get_user_from_google_id(google_id)
+    if user is not None:
+        return False
+    dbops.create_user(google_id)
+    return True
