7668a43f37e20539c4cfa9cc4aa46b48252a9003
erdem <erdem@elliot.ai>
2021-04-14 18:26:10 +0300

    New demo data

--- backend/demo_db.py
+++ backend/demo_db.py
@@ -81,6 +81,36 @@ def populate():
             country_id=4,
         )
     )
+    add(
+        User(
+            id=6,
+            google_id="115512136232290362553",
+            email="usluayberk1998@gmail.com",
+            name="Ayberk Uslu",
+            age=22,
+            country_id=1,
+        )
+    )
+    add(
+        User(
+            id=7,
+            google_id="118071647661695719427",
+            email="erdemkiraz@gmail.com",
+            name="Erdem Kirez",
+            age=22,
+            country_id=1,
+        )
+    )
+    add(
+        User(
+            id=8,
+            google_id="114491661639251135156",
+            email="korilipokybird@gmail.com",
+            name="Korili Pokybird",
+            age=19,
+            country_id=4,
+        )
+    )
 
     add(Vaccine(id=1, name="Covid-19 Biontech", required_dose=2))
     add(Vaccine(id=2, name="Covid-19 Moderna", required_dose=2))
@@ -106,6 +136,39 @@ def populate():
             vaccinated_at="Ankara",
             date=datetime.datetime(2021, 2, 22, 4),
             valid_until=datetime.datetime(2022, 2, 22, 4),
+            visibility=VaccinationVisibility.PUBLIC.value,
+        )
+    )
+    add(
+        Vaccination(
+            id=3,
+            user_id=6,
+            vaccine_id=1,
+            vaccinated_at="Ankara",
+            date=datetime.datetime(2021, 1, 3, 17),
+            valid_until=datetime.datetime(2022, 1, 3, 17),
+            visibility=VaccinationVisibility.PUBLIC.value,
+        )
+    )
+    add(
+        Vaccination(
+            id=4,
+            user_id=7,
+            vaccine_id=1,
+            vaccinated_at="Istanbul",
+            date=datetime.datetime(2021, 2, 3, 17),
+            valid_until=datetime.datetime(2022, 2, 3, 17),
+            visibility=VaccinationVisibility.PUBLIC.value,
+        )
+    )
+    add(
+        Vaccination(
+            id=5,
+            user_id=8,
+            vaccine_id=2,
+            vaccinated_at="Konya",
+            date=datetime.datetime(2021, 3, 4, 17),
+            valid_until=datetime.datetime(2022, 3, 4, 17),
             visibility=VaccinationVisibility.PUBLIC.value,
         )
     )
