ab09c37b9e558b68cf9d263fa48e0252cf17dc8d
erdem <erdem@elliot.ai>
2021-04-16 07:16:06 +0300

    Use secrets

--- backend/twilio_api.py
+++ backend/twilio_api.py
@@ -1,7 +1,8 @@
+import os
 from twilio.rest import Client
 
 account_sid = "ACcf93ddc15d0b48c8049cc305d24e0cb9"
-auth_token = "740301e4fee989c450cb4c3f467fbd0d"
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 number = "+15597174563"
 client = Client(account_sid, auth_token)
 
