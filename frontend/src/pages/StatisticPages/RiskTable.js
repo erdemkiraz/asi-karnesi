0bfd2fec608ec382d9f771400ee0a3c000c768d4
Ayberk Uslu <46554129+ayberkuslu@users.noreply.github.com>
2021-04-23 19:31:21 +0300

    Distributed ayberk (#70)
    * web bugfix : statistic bars are blocking statistic lines
    
    * statistic pages are updated, more background color option and css bug fixes
    
    * daily covid statistic numbers are moved to a json file
    
    * Create daily_covid_statistics.json

--- frontend/src/pages/StatisticPages/RiskTable.js
+++ frontend/src/pages/StatisticPages/RiskTable.js
@@ -8,10 +8,8 @@ export class RiskTable extends React.Component {
         super();
 
         this.state = {
-            basicData: {
-
-            },
-            is_fetched : false,
+            basicData: {},
+            is_fetched: false,
 
         };
 
@@ -64,41 +62,55 @@ export class RiskTable extends React.Component {
 
         for (let i = 0; i < data_array.length; i++) {
             labels[i] = data_array[i]["name"]
-            data_from_db[i] = data_array[i]["count"]*100
+            data_from_db[i] = data_array[i]["count"]
+        }
+
+
+        for (let i = 0; i < data_array.length; i++) {
+            let array = new Array(data_array.length).fill(0);
+            array[i] = data_from_db[i];
+            console.log(i , "th data : ", array)
+            datasets[i] = {
+                label: labels[i],
+                backgroundColor: this.getRiskColorByPercentage(data_from_db[i]),
+                data: array,
+            }
 
         }
-        datasets[0] = {
-            label: 'Risk Percentage %',
-            backgroundColor: '#B00020',
-            // backgroundColor: '#42A5F5',
-            data: data_from_db
-        }
+
 
         current_basic_data["labels"] = labels
         current_basic_data["datasets"] = datasets
         // console.log("-------------After > ")
 
         this.setState({basicData: current_basic_data})
-        this.setState({is_fetched : true})
-
-
+        this.setState({is_fetched: true})
 
     }
 
-    forceRender() {
-        let i = this.state.force_render;
-        this.setState({force_render: i + 1})
+    getRiskColorByPercentage(percentage){
+        if(percentage > .69){
+            return "#439529"
+        }
+        else if (percentage > .49){
+            return "#d2c31b"
+        }
+        else{
+            return '#ac132f'
+        }
+
     }
+
 
     render() {
 
         return (
-            <div style={{height: "700px" }}>
-                {   this.state.is_fetched &&
-                    <div className="card"  >
-                        <h5>Risk Table</h5>
-                        <Chart type="bar" data={this.state.basicData} options={this.basicOptions} style={{height : "900px !important", width:"2340px !important", display : "block !important"}} />
-                    </div>
+            <div style={{height: "700px"}}>
+                {this.state.is_fetched &&
+                <div className="card">
+                    <h5>Risk Table</h5>
+                    <Chart type="bar" data={this.state.basicData} options={this.basicOptions} />
+                </div>
 
                 }
             </div>
